ad212906d412054ad23e4faf111cbeff88433681
LauraHPG <87190606+LauraHPG@users.noreply.github.com>
2021-12-11 12:07:27 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-    for(let i = 0; i < 24; ++i){
+    for(let i = 0; i < 50; ++i){
         if(i%4 == 0){
             $('#prova').append(
                 `<a class="feina">About</a>`);
@@ -10,8 +10,3 @@ $(document).ready(function() {
     
     }
 });
-
-function getInputValue() {
-    let inputVal = document.getElementById("inputId").value;
-    //guardar inputVal a localStorage
-  }
